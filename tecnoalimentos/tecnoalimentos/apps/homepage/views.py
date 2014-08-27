6b8bed65bb98df92a9db14f40e84dd510a77ea79
JOSE ORDOÑEZ <restonjd@gmail.com>
2014-08-27 14:38:34 -0430

    contacto set email

--- tecnoalimentos/tecnoalimentos/apps/homepage/views.py
+++ tecnoalimentos/tecnoalimentos/apps/homepage/views.py
@@ -10,10 +10,7 @@ def index(request):
 	return render_to_response('homepage/index.html', context_instance=RequestContext(request))
 
 def aboutus(request):
-	mision = "misión de la empresa"
-	vision = "visión de la empresa"
-	ctx = {'mision': mision, 'vision': vision}
-	return render_to_response('homepage/quienesomos.html', ctx, context_instance=RequestContext(request))
+	return render_to_response('homepage/quienesomos.html', context_instance=RequestContext(request))
 
 def products(request):
 	return render_to_response('homepage/products.html', context_instance=RequestContext(request))
@@ -27,7 +24,7 @@ def contact(request):
 			cd = form.cleaned_data
 			asunto = u'Por: %s mail: %s ' % (cd['nombre'], cd['email'])
 			content = u'Email contacto: %s \nNombre: %s \nPais: %s \nEmpresa: %s \nTelefono: %s \nDescripcion: %s' % (cd['email'], cd['nombre'], cd['pais'], cd['empresa'], cd['telefono'], cd['texto'])
-			send_mail(asunto, content, cd['email'], ['info@duranjo.com'])
+			send_mail(asunto, content, 'no-reply@tecnoalimentosam2.com', ['info@tecnoalimentosam2.com'])
 	else:
 		form = contactForm()
 	ctx = {'form': form, 'success': success}
